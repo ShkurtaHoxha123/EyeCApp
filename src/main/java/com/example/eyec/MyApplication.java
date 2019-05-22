2e2cabe887743b39ea1aa252e99679d752f7fc79
ShkurtaHoxha123 <50530294+ShkurtaHoxha123@users.noreply.github.com>
2019-05-22 22:55:50 +0100

    Add files via upload

---
+++ src/main/java/com/example/eyec/MyApplication.java
@@ -0,0 +1,68 @@
+package com.example.eyec;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.service.BeaconManager;
+
+import java.util.List;
+import java.util.UUID;
+
+public class MyApplication extends Application {
+
+    public BeaconManager beaconManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        beaconManager = new BeaconManager(getApplicationContext());
+
+        beaconManager.setMonitoringListener(new BeaconManager.BeaconMonitoringListener() {
+
+            public BeaconRegion region;
+
+            public void onEnteredRegion(BeaconRegion beaconRegion, List<com.estimote.coresdk.recognition.packets.Beacon> beacons) {
+                showNotification(
+                        "Living Room",
+                        "You are in the Living Room");
+
+            }
+
+            public void onExitedRegion(BeaconRegion region) {
+            }
+        });
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+
+            public void onServiceReady() {
+                beaconManager.startMonitoring(new BeaconRegion(
+                        "monitored region",
+                        UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),
+                        9644, 36535));
+            }
+        });
+    }
+    public void showNotification(String title, String message) {
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+                new Intent[] { notifyIntent }, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+    }
+}
